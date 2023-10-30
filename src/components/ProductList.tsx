685b3e7f418d157bd6d8e9e1707e9096c0dfc3c6
JorgeHCamara <jorgehcamara@hotmail.com>
2023-10-30 09:09:25 -0300

    chore: product sorting added

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,13 +6,42 @@ import {
   TableHead,
   TableRow,
   Button,
+  IconButton,
 } from '@mui/material';
 import { Product } from '../interfaces/Product';
 import { getProducts } from '../services/ProductApi';
 import { deleteProduct } from '../services/ProductApi';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 const ProductList = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [sortField, setSortField] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+
+  const sortedProducts = [...products].sort((a, b) => {
+    if (!sortField) return 0;
+    
+    const key = sortField as keyof Product;
+    
+    const aValue = typeof a[key] === 'string' ? (a[key] as string).toLowerCase() : a[key];
+    const bValue = typeof b[key] === 'string' ? (b[key] as string).toLowerCase() : b[key];
+  
+    if (aValue < bValue) return sortOrder === 'asc' ? -1 : 1;
+    if (aValue > bValue) return sortOrder === 'asc' ? 1 : -1;
+    return 0;
+  });
+  
+
+const handleSort = (field: keyof Product) => {
+  if (sortField === field) {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  } else {
+    setSortField(field);
+    setSortOrder('asc');
+  }
+};
+
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -44,18 +73,48 @@ const ProductList = () => {
   return (
     <Table>
       <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Description</TableCell>
-          <TableCell>Color</TableCell>
-          <TableCell>Product Type</TableCell>
-          <TableCell>Price</TableCell>
-          <TableCell>Promotional Price</TableCell>
-          <TableCell>#</TableCell>
-        </TableRow>
+          <TableRow>
+              <TableCell onClick={() => handleSort('name')}>
+                  Name
+                  <IconButton size="small">
+                    {sortField === 'name' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell onClick={() => handleSort('description')}>
+                  Description
+                  <IconButton size="small">
+                    {sortField === 'description' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell onClick={() => handleSort('color')}>
+                  Color
+                  <IconButton size="small">
+                    {sortField === 'color' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell onClick={() => handleSort('category')}>
+                  Product Type
+                  <IconButton size="small">
+                    {sortField === 'category' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell onClick={() => handleSort('price')}>
+                  Price
+                  <IconButton size="small">
+                    {sortField === 'price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell onClick={() => handleSort('promotional_price')}>
+                  Promotional Price
+                  <IconButton size="small">
+                    {sortField === 'promotional_price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                  </IconButton>
+              </TableCell>
+              <TableCell>#</TableCell>
+          </TableRow>
       </TableHead>
       <TableBody>
-        {products.map(product => (
+        {sortedProducts.map(product => (
           <TableRow key={product.id}>
             <TableCell>{product.name}</TableCell>
             <TableCell>{product.description}</TableCell>
