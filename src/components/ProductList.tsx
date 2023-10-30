a2c92d18ab0f2940fbe01131b93a2373829b331e
JorgeHCamara <jorgehcamara@hotmail.com>
2023-10-30 09:46:37 -0300

    chore: filter products by name or product type

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -7,17 +7,23 @@ import {
   TableRow,
   Button,
   IconButton,
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
 } from '@mui/material';
 import { Product } from '../interfaces/Product';
-import { getProducts } from '../services/ProductApi';
-import { deleteProduct } from '../services/ProductApi';
+import { getProducts, deleteProduct } from '../services/ProductApi';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import './ProductList.css'
 
 const ProductList = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [sortField, setSortField] = useState<string | null>(null);
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [nameFilter, setNameFilter] = useState('');
+  const [categoryFilter, setCategoryFilter] = useState('');
 
   const sortedProducts = [...products].sort((a, b) => {
     if (!sortField) return 0;
@@ -31,16 +37,20 @@ const ProductList = () => {
     if (aValue > bValue) return sortOrder === 'asc' ? 1 : -1;
     return 0;
   });
-  
 
-const handleSort = (field: keyof Product) => {
-  if (sortField === field) {
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-  } else {
-    setSortField(field);
-    setSortOrder('asc');
-  }
-};
+  const filteredProducts = sortedProducts.filter(product => {
+    return product.name.toLowerCase().includes(nameFilter.toLowerCase()) && 
+           product.category.toLowerCase().includes(categoryFilter.toLowerCase());
+  });
+
+  const handleSort = (field: keyof Product) => {
+    if (sortField === field) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortField(field);
+      setSortOrder('asc');
+    }
+  };
 
 
   useEffect(() => {
@@ -71,66 +81,101 @@ const handleSort = (field: keyof Product) => {
   }
 
   return (
-    <Table>
-      <TableHead>
-          <TableRow>
-              <TableCell onClick={() => handleSort('name')}>
-                  Name
-                  <IconButton size="small">
-                    {sortField === 'name' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell onClick={() => handleSort('description')}>
-                  Description
-                  <IconButton size="small">
-                    {sortField === 'description' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell onClick={() => handleSort('color')}>
-                  Color
-                  <IconButton size="small">
-                    {sortField === 'color' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell onClick={() => handleSort('category')}>
-                  Product Type
-                  <IconButton size="small">
-                    {sortField === 'category' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell onClick={() => handleSort('price')}>
-                  Price
-                  <IconButton size="small">
-                    {sortField === 'price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell onClick={() => handleSort('promotional_price')}>
-                  Promotional Price
-                  <IconButton size="small">
-                    {sortField === 'promotional_price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
-                  </IconButton>
-              </TableCell>
-              <TableCell>#</TableCell>
-          </TableRow>
-      </TableHead>
-      <TableBody>
-        {sortedProducts.map(product => (
-          <TableRow key={product.id}>
-            <TableCell>{product.name}</TableCell>
-            <TableCell>{product.description}</TableCell>
-            <TableCell>{product.color}</TableCell>
-            <TableCell>{product.category}</TableCell>
-            <TableCell>${(product.price).toFixed(2)}</TableCell>
-            <TableCell>${mathFloor(product.promotional_price).toFixed(2)}</TableCell>
-            <TableCell>
-              <Button variant="outlined" color="warning" onClick={() => handleDeleteProduct(product.id)}>
-                Delete
-              </Button>
-          </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <div className="filter-section">
+        <InputLabel>
+          Filter by Name
+        </InputLabel>
+        <TextField 
+          variant="outlined"
+          value={nameFilter}
+          onChange={(e) => setNameFilter(e.target.value)}
+        />
+        <InputLabel>
+          Product Type
+        </InputLabel>
+        <Select 
+          label="Filter by Category"
+          variant="outlined"
+          className="categoryInput"
+          value={categoryFilter}
+          onChange={(e) => setCategoryFilter(e.target.value)}
+        >
+          <MenuItem value="Appliances">Appliances</MenuItem>
+          <MenuItem value="Furniture">Furniture</MenuItem>
+          <MenuItem value="Refrigerators">Refrigerators</MenuItem>
+          <MenuItem value="Smartphones">Smartphones</MenuItem>
+          <MenuItem value="Electronics">Electronics</MenuItem>
+        </Select>
+        <Button onClick={() => {
+          setNameFilter('');
+          setCategoryFilter('');
+        }}>
+          Clear Filters
+        </Button>
+      </div>
+      <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell onClick={() => handleSort('name')}>
+                    Name
+                    <IconButton size="small">
+                      {sortField === 'name' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell onClick={() => handleSort('description')}>
+                    Description
+                    <IconButton size="small">
+                      {sortField === 'description' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell onClick={() => handleSort('color')}>
+                    Color
+                    <IconButton size="small">
+                      {sortField === 'color' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell onClick={() => handleSort('category')}>
+                    Product Type
+                    <IconButton size="small">
+                      {sortField === 'category' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell onClick={() => handleSort('price')}>
+                    Price
+                    <IconButton size="small">
+                      {sortField === 'price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell onClick={() => handleSort('promotional_price')}>
+                    Promotional Price
+                    <IconButton size="small">
+                      {sortField === 'promotional_price' && sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="inherit" /> : <ArrowDownwardIcon fontSize="inherit" />}
+                    </IconButton>
+                </TableCell>
+                <TableCell>#</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredProducts.map(product => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.description}</TableCell>
+              <TableCell>{product.color}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>${(product.price).toFixed(2)}</TableCell>
+              <TableCell>${mathFloor(product.promotional_price).toFixed(2)}</TableCell>
+              <TableCell>
+                <Button variant="outlined" color="warning" onClick={() => handleDeleteProduct(product.id)}>
+                  Delete
+                </Button>
+            </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+    
   );
 }
 
